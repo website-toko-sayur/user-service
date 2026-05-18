b03f7c76fba80e6e3b46e661a2e2e97498572f1e
xylo <m.aditya3232@gmail.com>
2026-05-18 12:00:16 +0700

    add middleware gateway validation

--- go.mod
+++ go.mod
@@ -9,10 +9,15 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.7.0 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/gofiber/fiber/v3 v3.2.0 // indirect
+	github.com/gofiber/schema v1.7.1 // indirect
+	github.com/gofiber/utils/v2 v2.0.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -26,8 +31,12 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.26 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20250401214520-65e299d6c5c9 // indirect
+	github.com/tinylib/msgp v1.6.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.70.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 )
 
---
+++ internal/middleware/gateway_validation.go
@@ -0,0 +1,69 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/rs/zerolog/log"
+)
+
+func GatewayValidationMiddleware() fiber.Handler {
+	requireGateway := os.Getenv("REQUEST_API_GATEWAY")
+	if requireGateway == "" {
+		requireGateway = "true"
+	}
+
+	expectedGateway := os.Getenv("GATEWAY_SECRET_KEY")
+
+	return func(c fiber.Ctx) error {
+		if requireGateway == "false" {
+			return c.Next()
+		}
+
+		if c.Path() == "/health" {
+			return c.Next()
+		}
+
+		gatewayHeader := c.Get("X-API-Gateway")
+		if gatewayHeader != "true" {
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"status":  "error",
+				"message": "Access denied. Request must go through API Gateway.",
+				"code":    "GATEWAY_REQUIRED",
+			})
+		}
+
+		if expectedGateway != "" {
+			receivedSecret := c.Get("X-Gateway-Secret")
+
+			if receivedSecret != expectedGateway {
+				return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+					"status":  "error",
+					"message": "Invalid gateway secret key.",
+					"code":    "INVALID_GATEWAY_SECRET",
+				})
+			}
+		}
+
+		gatewayVersion := c.Get("X-API-Gateway-Version")
+		if gatewayVersion == "" {
+			log.Warn().
+				Str("source", "internal.middleware.GatewayValidationMiddleware").
+				Msg("Missing X-API-Gateway-Version header")
+
+		}
+
+		secretStatus := "not configured"
+		if expectedGateway != "" {
+			secretStatus = "validated"
+		}
+
+		log.Info().
+			Str("gateway_version", gatewayVersion).
+			Str("request_id", c.Get("X-Request-ID")).
+			Str("secret_status", secretStatus).
+			Msg("Request validated from API Gateway")
+
+		return c.Next()
+	}
+}
