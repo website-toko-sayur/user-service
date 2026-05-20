a524798933f2fedd640de745eb0a3c36be3320d7
xylo <m.aditya3232@gmail.com>
2026-05-20 17:15:37 +0700

    fix cost hash password to 8 biar cepet

--- utils/conv/conv.go
+++ utils/conv/conv.go
@@ -2,17 +2,26 @@ package conv
 
 import (
 	"strconv"
+	"time"
 
+	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 8)
 	return string(bytes), err
 }
 
 func CheckPasswordHash(password, hash string) bool {
+	start := time.Now()
+
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	log.Info().
+		Dur("duration", time.Since(start)).
+		Msg("bcrypt compare")
+
 	return err == nil
 }
 
