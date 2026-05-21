9f08e5d12479ac51a65a4130042276567228b28c
xylo <m.aditya3232@gmail.com>
2026-05-21 17:24:37 +0700

    change resp error to fiber

--- internal/adapter/handler/response/default_response.go
+++ internal/adapter/handler/response/default_response.go
@@ -1,10 +1,5 @@
 package response
 
-import (
-	"github.com/gofiber/fiber/v3"
-	"github.com/rs/zerolog/log"
-)
-
 type DefaultResponse struct {
 	Message string `json:"message"`
 	Data    any    `json:"data"`
@@ -21,22 +16,4 @@ type Pagination struct {
 	TotalCount int64 `json:"total_count"`
 	PerPage    int64 `json:"per_page"`
 	TotalPage  int64 `json:"total_page"`
-}
-
-func RespondWithError(c fiber.Ctx, code int, context string, err error) error {
-	log.Error().
-		Err(err).
-		Str("context", context).
-		// Str("method", c.Method()).
-		// Str("path", c.Path()).
-		// Str("ip", c.IP()).
-		Str("source", "internal.adapter.handler.response.RespondWithError").
-		Msg("request failed")
-
-	resp := DefaultResponse{
-		Message: err.Error(),
-		Data:    nil,
-	}
-
-	return c.Status(code).JSON(resp)
 }
