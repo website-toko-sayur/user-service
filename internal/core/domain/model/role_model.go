18a7e63750a24ad272793de2ff74a93e1d6d87f9
xylo <m.aditya3232@gmail.com>
2026-06-06 15:51:16 +0700

    add partial unique index in role name unique key

--- internal/core/domain/model/role_model.go
+++ internal/core/domain/model/role_model.go
@@ -14,3 +14,14 @@ type Role struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 	Users     []User         `gorm:"many2many:user_role"` // merujuk table pivot, user_role -> penghubung antara user & role
 }
+
+/*
+ buat partial unique index, agar unique index hanya berlaku untuk data yang belum dihapus
+
+ ALTER TABLE roles
+     DROP CONSTRAINT IF EXISTS uni_roles_name;
+
+ CREATE UNIQUE INDEX uni_roles_name
+     ON roles (name)
+     WHERE deleted_at IS NULL;
+*/
